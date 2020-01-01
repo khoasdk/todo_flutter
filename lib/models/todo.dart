117b700ae24fbeb354798835af82ab072edcf14d
Khoa Vo <khoa.sdk@gmail.com>
2020-01-01 10:56:32 +0700

    Add Todo model

---
+++ lib/models/todo.dart
@@ -0,0 +1,34 @@
+class Todo {
+  int _id;
+  String _title;
+  String _description;
+  int _priority;
+  String _date;
+
+  Todo(this._title, this._date, [this._description]);
+  Todo.withId(this._id, this._title, this._date, [this._description]);
+
+  int get id => _id;
+  String get title => _title;
+  String get description => _description;
+  String get date => _date;
+  int get priority => _priority;
+
+  set title(String value) {
+    if (value.length <= 255) this._title = value;
+  }
+  set description(String value) {
+    if (value.length <= 255) this._description = value;
+  }
+  set date(String value) {
+    this._date = value;
+  }
+
+  Todo.fromMap(Map<String, dynamic> map) {
+    _id = map['id'];
+    _title = map['title'];
+    _description = map['desciption'];
+    _priority = map['priority'];
+    _date = map['date'];
+  }
+}
