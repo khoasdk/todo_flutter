e9a662e0fe1bca2008aa920fdef15c407a7968da
Khoa Vo <khoa.sdk@gmail.com>
2020-01-01 11:03:46 +0700

    Update Todo model

--- lib/models/todo.dart
+++ lib/models/todo.dart
@@ -31,4 +31,14 @@ class Todo {
     _priority = map['priority'];
     _date = map['date'];
   }
+
+  Map<String, dynamic> toMap() {
+    var map = Map<String, dynamic>();
+    if (id != null) map['id'] = _id;
+    map['title'] = _title;
+    map['description'] = _description;
+    map['priority'] = _priority;
+    map['date'] = _date;
+    return map;
+  }
 }
